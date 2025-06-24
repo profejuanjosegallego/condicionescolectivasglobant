59034d9e3f2d5af179010cae7ab8c6adef0ad926
josejesus823 <>
2025-06-24 07:07:55 -0500

    feat: se configura archivo condicional

---
+++ src/main/java/org/example/hidroituangoJJV/hidroituangoJJV.java
@@ -0,0 +1,14 @@
+package org.example.hidroituangoJJV;
+
+import java.util.Scanner;
+
+public class hidroituangoJJV {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String usuarioRegistrado = "SCHVELEZ";
+        String contraUsuarioRegistrado = "admin123*";
+
+
+    }
+}
