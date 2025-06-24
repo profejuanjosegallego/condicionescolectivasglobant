16e89bf6689df85a8ff1f6ec4a135d847f6ba5f3
jaco0010 <jacovillegas12@gmail.com>
2025-06-24 07:08:28 -0500

    feat se configura archivo condicional

---
+++ src/main/java/org/example/HidroituangoJGV/HidroituangoJGV.java
@@ -0,0 +1,15 @@
+package org.example.HidroituangoJGV;
+
+import java.util.Scanner;
+
+public class HidroituangoJGV {
+
+    public static void main(String[] args) {
+
+        Scanner leerTeclado = new Scanner(System.in);
+        String usuarioRegistrado= "SCHVELEZ";
+        String contraseñaUsuarioRegistrado="admin123*";
+
+        
+    }
+}
