1477675d6e315ac3487224bd32a81fe7b7919e0c
Alejandro <Diegoalejandro9803@hotmail.com>
2025-06-24 07:08:30 -0500

    feat se configura archivo condicional

---
+++ src/main/java/HidroItuangoDAOC/HidroItuangoDAOC.java
@@ -0,0 +1,15 @@
+package HidroItuangoDAOC;
+
+import java.util.Scanner;
+
+public class HidroItuangoDAOC {
+
+        public static void main(String[] args) {
+
+            Scanner leerTeclado = new Scanner(System.in);
+            Scanner usuarioRegistrado = "SCHVELEZ";
+            String contraseñaUsuarioRegistrado = "admin123*";
+            
+    }
+
+}
