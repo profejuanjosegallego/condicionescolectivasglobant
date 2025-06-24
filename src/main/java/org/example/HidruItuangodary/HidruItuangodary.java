47ac3c9732f77d32ed7acba81514919e5e60ebff
cely <kevinlaverde3@gmail.com>
2025-06-24 07:11:10 -0500

    feat se configura archivo condicional

---
+++ src/main/java/org/example/HidruItuangodary/HidruItuangodary.java
@@ -0,0 +1,16 @@
+package org.example.HidruItuangodary;
+
+import java.util.Scanner;
+
+public class HidruItuangodary {
+
+    public static void main(String[] args) {
+
+        Scanner LeerTeclado = new Scanner(System.in);
+        String usuarioRegistrado="SCHVELEZ";
+        String contrasenaUsuarioRegistrado="admind123*";
+
+
+    }
+
+}
