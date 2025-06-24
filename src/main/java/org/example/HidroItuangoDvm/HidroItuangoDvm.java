275836473f13e3c207600028ce76c39203b43ca3
alexis <romero.carlos6357@gmail.com>
2025-06-24 07:08:28 -0500

    feat se configura archivo condicional

---
+++ src/main/java/org/example/HidroItuangoDvm/HidroItuangoDvm.java
@@ -0,0 +1,15 @@
+package org.example.HidroItuangoDvm;
+
+import java.util.Scanner;
+
+public class HidroItuangoDvm {
+
+    public static void main(String[] args) {
+        Scanner leerTeclado = new Scanner(System.in);
+        String usuarioRegistrado= "SCHVELEZ";
+        String contraseñaUsuarioRegistrado="admin123*";
+        
+    }
+}
+
+
