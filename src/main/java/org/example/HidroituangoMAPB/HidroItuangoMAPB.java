42837c1c4f63c887a5dd3011f0522a7395a495d6
MateoPinedaBel <MateoPinedaBel@Outlook.com>
2025-06-24 07:08:29 -0500

    Feat: Se configura archivo condicional.

---
+++ src/main/java/org/example/HidroituangoMAPB/HidroItuangoMAPB.java
@@ -0,0 +1,15 @@
+package org.example.HidroituangoMAPB;
+
+import java.util.Scanner;
+
+public class HidroItuangoMAPB {
+
+    public static void main(String[] args) {
+
+        // Definición de Variables
+
+        Scanner leerTeclado = new Scanner(System.in);
+        String usuarioRegistrado = "SCHVELEZ";
+        String contraseñaUsuarioRegistrado = "admin123*";
+    }
+}
