5b611b5282cace6b8a8a92cbe8d6981e7f79ecd5
andresmonterom17@gmail.com <tvandres@gamil.com>
2025-06-24 07:10:08 -0500

    feat se configura archivo condicional

---
+++ src/main/java/org/example/Hidroituango_Macs/HidroituangoMacs.java
@@ -0,0 +1,16 @@
+package org.example.Hidroituango_Macs;
+
+import java.util.Scanner;
+
+public class HidroituangoMacs {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Scanner LeerTeclado=new Scanner(System.in);
+
+        String usuarioRegistrado="SCHVELEZ";
+        String contraseñaUsuarioRegistrado="Admin123";
+        
+
+    }
+}
