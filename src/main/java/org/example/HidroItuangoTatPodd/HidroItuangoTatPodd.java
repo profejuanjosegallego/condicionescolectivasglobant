991a20810db411ccfe342483b347e18f20e57d5d
pussy destroyer <dcanaveral4.5@gmail.com>
2025-06-24 07:08:31 -0500

    feat se configura archivo condicional

---
+++ src/main/java/org/example/HidroItuangoTatPodd/HidroItuangoTatPodd.java
@@ -0,0 +1,16 @@
+package org.example.HidroItuangoTatPodd;
+
+import java.util.Scanner;
+
+public class HidroItuangoTatPodd {
+
+    public static void main(String[] args) {
+
+        Scanner leerTeclado = new Scanner(System.in);
+        String usuarioRegistrado = "SCHVELEZ";
+        String constraseñaUsuarioRegistro = "admin123*";
+        
+    }
+}
+
+
