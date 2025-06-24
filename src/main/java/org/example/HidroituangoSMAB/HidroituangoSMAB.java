19cb5fae5f2d910a9936c19435a86b3f9c43b57e
desarrollo <celindacorrea@iefincalamesa.edu.co>
2025-06-24 07:08:27 -0500

    feat se crea archivo

---
+++ src/main/java/org/example/HidroituangoSMAB/HidroituangoSMAB.java
@@ -0,0 +1,15 @@
+package org.example.HidroituangoSMAB;
+
+import java.util.Scanner;
+
+public class HidroituangoSMAB {
+        public static void main(String[] args) {
+
+            Scanner readData = new Scanner(System.in);
+            String usuarioRegistrado = "SCHVELEZ";
+            String contraseñaUsuarioRegistrado = "admin123*";
+            
+
+        }
+    }
+
